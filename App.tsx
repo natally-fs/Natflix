9cbedd3c526f049126bfd5f7010547bbba2b9e29
natally-fs <natallyfernandes.silva@gmail.com>
2023-02-13 18:15:57 -0300

    feat:  setup react navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Text, SafeAreaView, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function App(): JSX.Element {
+function HomeScreen() {
   return (
     <SafeAreaView>
       <StatusBar
@@ -13,6 +15,18 @@ function App(): JSX.Element {
       />
       <Text>Natflix</Text>
     </SafeAreaView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
